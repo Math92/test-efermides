e822537920d7140510ed12db34cfa1133cf92021
Math92 <mathiassan92@gmail.com>
2024-01-04 15:48:57 -0300

    eliminating call to text translation api

--- script.js
+++ script.js
@@ -17,40 +17,6 @@ document.getElementById('fetchButton').addEventListener('click', function () {
     fetchNumberFact(month, day);
 });
 
-async function translateText(text) {
-    const target_language = "es";
-    const source_language = "en";
-
-    const url = "https://text-translator2.p.rapidapi.com/translate";
-    const data = {
-        text: text,
-        target_language: target_language,
-        source_language: source_language
-    };
-
-
-    try {
-        const response = await fetch(url, {
-            method: "POST",
-            headers: {
-                "content-type": "application/x-www-form-urlencoded",
-                "X-RapidAPI-Key": "a71e103d4bmsh2d23398c391c2ddp1690cajsn1bc2aaf2288a",
-                "X-RapidAPI-Host": "text-translator2.p.rapidapi.com"
-            },
-            body: new URLSearchParams(data)
-        });
-
-        const jsonData = await response.json();
-        if (jsonData.status === "success" && jsonData.data.translatedText) {
-            return jsonData.data.translatedText;
-        } else {
-            return 'Traducción no disponible';
-        }
-    } catch (error) {
-        console.error('Error:', error);
-        return 'Error al traducir';
-    }
-}
 
 function displayFacts(facts) {
     const resultsContainer = document.getElementById('resultsContainer'); // Asegúrate de que este es el ID correcto
@@ -116,8 +82,7 @@ async function fetchNumberFact(month, day) {
             const fullText = `${fact} (Año: ${year}).`; // Texto completo del hecho
 
             if (!factsSet.has(fullText)) { // Comprobar si el hecho ya está en el Set
-                const translatedFact = await translateText(fullText);
-                factsSet.add(translatedFact); // Añadir hecho traducido al Set
+                factsSet.add(fullText); // Añadir hecho al Set
             }
         } catch (error) {
             console.error(error);
